81698524f415f4b3f56d3d17abd4637651a1ca74
Wasdom Kung <seken@gmx.com>
2017-09-08 15:55:31 +0100

    Additional guide files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
 import { UserItemComponent } from './user-item/user-item.component';
-//import { UserListComponent } from '/user-item/user-list.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HelloWorldComponent,
     UserItemComponent,
-    //UserListComponent,
+    UserListComponent,
   ],
   imports: [
     BrowserModule
