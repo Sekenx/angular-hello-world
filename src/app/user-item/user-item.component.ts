46b513f7fd896869859c8bd9e4e9efdb88d3bc44
Wasdom Kung <seken@gmx.com>
2017-09-08 13:58:01 +0100

    Hello World
    Short guide from Angular4

---
+++ src/app/user-item/user-item.component.ts
@@ -0,0 +1,13 @@
+import { OnInit } from '@angular/core';
+
+export class UserItemComponent implements OnInit {
+  name: string;
+
+  constructor() {
+    this.name = 'Felipe';
+  }
+
+  ngOnInit() {
+
+  }
+}
